31671880cb8b139cac678cd41c01c24e19e46edb
dmiaffo <46670911+dmiaffo@users.noreply.github.com>
2019-01-14 10:28:33 +0100

    Eclipse IDE – prise en main
    Télécharger et Installer Eclipse IDE – prise en main

---
+++ ClassTuto.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package net.miaffo.tuto;
+
+/**
+ * @author MIAFFO - https://miaffo.net
+ *
+ */
+public class ClassTuto {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  for(int position = 0; position < 10 ; position ++)
+  {
+	  if(position%2==0) System.out.println(position + " est un nombre pair");
+	  else  System.out.println(position + " est un nombre impair");
+  }
+
+	}
+
+}
